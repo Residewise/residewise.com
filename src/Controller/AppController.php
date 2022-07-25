fb44451bf554d7dcecb8a8b04cb9a50436288747
Kerrial Newham <kerrialnewham@Kerrials-MacBook-Pro.local>
2022-07-25 12:16:44 +0200

    update

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -8,7 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/app')]
 class AppController extends AbstractController
 {
     #[Route(path: '/privacy', name: 'app_privacy', methods: ['GET'])]
